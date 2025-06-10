581293a2871283718c310c8bac566f304e19b7cd
Quan Pham <qmpham2019@gmail.com>
2025-06-10 12:33:01 -0400

    Fetch Lenovo SU charge rates from `nerc_rates`
    H100 GPUs have also been added to the Lenovo invoice
    
    Related: https://github.com/CCI-MOC/nerc-rates/issues/35

--- process_report/tests/unit/processors/test_lenovo_processor.py
+++ process_report/tests/unit/processors/test_lenovo_processor.py
@@ -6,17 +6,23 @@ from process_report.tests import util as test_utils
 
 class TestLenovoProcessor(TestCase):
     def test_process_lenovo(self):
+        test_su_charge_info = {"GPUA100SXM4": 1, "GPUH100": 2.5}
         test_invoice = pandas.DataFrame(
             {
+                "SU Type": ["OpenStack GPUA100SXM4"] * 2
+                + ["OpenShift GPUA100SXM4"] * 2
+                + ["Not Lenovo SU", "Random GPUH100"],
                 "SU Hours (GBhr or SUhr)": [1, 10, 100, 4, 432, 10],
             }
         )
         answer_invoice = test_invoice.copy()
-        answer_invoice["SU Charge"] = 1
+        answer_invoice["SU Charge"] = [1, 1, 1, 1, 0, 2.5]
         answer_invoice["Charge"] = (
             answer_invoice["SU Hours (GBhr or SUhr)"] * answer_invoice["SU Charge"]
         )
 
-        lenovo_proc = test_utils.new_lenovo_processor(data=test_invoice)
+        lenovo_proc = test_utils.new_lenovo_processor(
+            data=test_invoice, su_charge_info=test_su_charge_info
+        )
         lenovo_proc.process()
         self.assertTrue(lenovo_proc.data.equals(answer_invoice))
